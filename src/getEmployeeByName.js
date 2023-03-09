3aae33a69868b74a3d3082f4b674520f0ab89c57
caue peruque <caue19052003@gmail.com>
2023-03-09 17:54:11 -0300

    requisito 4 finalizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return data.employees
+    .find(({ firstName, lastName }) => (firstName === employeeName) || (lastName === employeeName));
 };
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const isManager = (id) => employees
+  .some((employee) => employee.managers
+    .some((managerId) => managerId === id));
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const peoples = employees.filter((employee) => employee.managers
+    .some((manager) => manager === managerId))
+    .map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  return peoples;
 };
 
 module.exports = { isManager, getRelatedEmployees };
