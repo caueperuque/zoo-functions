033a8a575f6be0015144ff08d84cab6b7dc6378c
caue peruque <caue19052003@gmail.com>
2023-03-10 17:28:20 -0300

    requisito 5 finalizado GRAÇAS A DEUS EU NAO AGUENTAVA AMIS

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
+// const countAnimals = (animal) => {
+//   if (animal === undefined) {
+//     data.species.filter((specie) => specie.residents.reduce((acc, cur) =>
+//     {
+//       acc[cur] = specie.residents.map((resident) => resident.name).length;
+//       return acc;
+//     },{}));
+//   }
+// };
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, { name }) => {
+      acc[name] = data.species
+        .find((specie) => specie.name === name).residents
+        .map((resident) => resident.name).length;
+      return acc;
+    }, {});
+  }
+  if (animal.sex === undefined) {
+    return data.species
+      .find(({ name }) => name === animal.species).residents
+      .map((resident) => resident.name).length;
+  }
+  return data.species
+    .find(({ name }) => name === animal.species).residents
+    .filter(({ sex }) => sex === animal.sex).length;
 };
+
+console.log(countAnimals({ species: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
