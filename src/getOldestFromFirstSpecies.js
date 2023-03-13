8b313772f3d9be382962353346ccffb708e542dd
caue peruque <caue19052003@gmail.com>
2023-03-13 01:51:20 -0300

    requisito 10 finalizado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employee = data.employees.find((employees) => employees.id === id);
+  const responsibleFor = employee.responsibleFor[0];
+  const animal = species.find((specie) => specie.id === responsibleFor);
+  const ageAnimal = animal.residents.map(({ age }) => age).sort((a, b) => b - a);
+  const oldestAnimal = animal.residents.find(({ age }) => age === ageAnimal[0]);
+  return Object.values(oldestAnimal);
 };
-
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
